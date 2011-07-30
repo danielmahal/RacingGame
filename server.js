b9fe370a5b28590f4b69f605e6b5d010a483d41c
Daniel <daniel@unfold.no>
2011-07-30 21:10:38 +0200

    changed port back to 3000

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/maps/:map/collision', function(req, res){
 
 
 
-app.listen(8362);
+app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 var socket = socket.start(app);
