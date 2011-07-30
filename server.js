e8e0a1f082420c0d08494c0ea1f28205918cb3e3
Daniel <daniel@unfold.no>
2011-07-30 21:00:29 +0200

    changed port on server to 80

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/maps/:map/collision', function(req, res){
 
 
 
-app.listen(3000);
+app.listen(80);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 var socket = socket.start(app);
